0c3e2ab8c577fd0b7b5c82766e36be453c1d05ca
Denisimus <idenisimus88@gmail.com>
2021-05-17 19:37:13 +0300

    styled main page

--- src/components/profileInfo.js
+++ src/components/profileInfo.js
@@ -1,10 +1,28 @@
-import { StyledProfileInfoSection } from "../styles";
+import {
+  StyledProfileInfoSection,
+  StyledProfilePhoto,
+  StyledProfileData,
+  ProfilePhotoWrapper,
+  ProfileDataWrapper,
+  StyledProfileDataName,
+  StyledProfileDataStatus,
+} from "../styles";
 
 export const ProfileInfo = () => {
   return (
     <StyledProfileInfoSection>
-      <div>Photo</div>
-      <div>info</div>
+      <ProfilePhotoWrapper>
+        <StyledProfilePhoto
+          src="https://i.pinimg.com/originals/e8/83/7b/e8837b00067930f307a825c2ff74f3fa.jpg"
+          alt="profile-photo"
+        ></StyledProfilePhoto>
+      </ProfilePhotoWrapper>
+      <ProfileDataWrapper>
+        <StyledProfileData>
+          <StyledProfileDataName>Светлана Иванова</StyledProfileDataName>
+          <StyledProfileDataStatus>Тут будет статус</StyledProfileDataStatus>
+        </StyledProfileData>
+      </ProfileDataWrapper>
     </StyledProfileInfoSection>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -1,19 +1,67 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #EDEEF0;
+  }
+`;
+
+export const MainPageWrapper = styled.div`
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  width: 100%;
+  max-width: 800px;
+`;
 
 export const StyledNavigation = styled.nav`
-  background-color: red;
+  font-size: 20px;
+  margin-left: 10px;
 `;
 
 export const StyledNavigationItem = styled.li`
   list-style: none;
+  margin-top: 10px;
 `;
 
-export const MainPageWrapper = styled.div`
-  display: flex;
+export const ProfilePageWrapper = styled.div`
+  margin-left: 10px;
 `;
-
-export const ProfilePageWrapper = styled.div``;
 
 export const StyledProfileInfoSection = styled.section`
   display: flex;
+`;
+
+export const ProfilePhotoWrapper = styled.div`
+  max-width: 300px;
+  min-width: 200px;
+  background-color: white;
+  border-radius: 2%;
+`;
+
+export const StyledProfilePhoto = styled.img`
+  margin: 10px;
+  width: 200px;
+  background-size: cover;
+`;
+
+export const ProfileDataWrapper = styled.div`
+  margin-left: 10px;
+  width: 400px;
+  background-color: white;
+  border-radius: 2%;
+`;
+
+export const StyledProfileData = styled.div``;
+
+export const StyledProfileDataName = styled.h2`
+  margin: 10px;
+`;
+
+export const StyledProfileDataStatus = styled.p`
+  ::after {
+    content: " 🦄";
+  }
+  margin: 10px;
 `;
